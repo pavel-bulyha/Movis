3930bbcf1dec8903fe467bf1544ea5820117acd0
Pavel Bulyha <pavel.bulyha@gmail.com>
2021-03-17 23:02:27 +0300

    commit №1

---
+++ app/src/main/java/com/example/movis/Movis.kt
@@ -0,0 +1,11 @@
+package com.example.movis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movis : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
